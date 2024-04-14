04d25a75e1081899f40633f72b76d186a5aede8d
inuyasha <yingzijiany@gmail.com>
2024-04-14 22:50:21 +0200

    City entity angelegt

--- src/main/java/de/ostfalia/bootablejarstarter/entity/City.java
+++ src/main/java/de/ostfalia/bootablejarstarter/entity/City.java
@@ -1,14 +1,70 @@
 package de.ostfalia.bootablejarstarter.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "city")
 public class City {
     @Id
-    @Column(name = "city_id")
+    @Column(name = "city_id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer cityId;
+
+    @Column(length = 50,nullable = false)
+    private String city;
+
+    @JoinColumn(name = "country_id",nullable = false)
+    @ManyToOne
+    private Country country;
+
+    @Column(name = "last_update",nullable = false)
+    private LocalDateTime lastUpdate;
+    @PrePersist
+    @PreUpdate
+    private void onUpdate() {
+        lastUpdate = LocalDateTime.now();
+    }
+
+    public City() {
+    }
+
+    public City(String city, Country country, LocalDateTime lastUpdate) {
+        this.city = city;
+        this.country = country;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
