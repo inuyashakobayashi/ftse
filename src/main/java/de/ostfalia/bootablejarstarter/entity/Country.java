08867d1bc8afd57d04181cbb2bb25b26012fd25c
inuyasha <yingzijiany@gmail.com>
2024-04-14 22:50:21 +0200

    City entity angelegt

--- src/main/java/de/ostfalia/bootablejarstarter/entity/Country.java
+++ src/main/java/de/ostfalia/bootablejarstarter/entity/Country.java
@@ -1,4 +1,12 @@
 package de.ostfalia.bootablejarstarter.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "country")
 public class Country {
+    @Id
+    @Column(name = "country_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer countryId;
 }
