3202220c6ae2da563eec2dda339af561e8371d0f
inuyasha <yingzijiany@gmail.com>
2024-04-15 14:59:08 +0200

    Rental angelegt

--- src/main/java/de/ostfalia/bootablejarstarter/entity/Rental.java
+++ src/main/java/de/ostfalia/bootablejarstarter/entity/Rental.java
@@ -16,4 +16,91 @@ public class Rental {
    @JoinColumn(name = "inventory_id",nullable = false)
 @ManyToOne
     private Inventory inventory;
+
+   @JoinColumn(name = "customer_id",nullable = false)
+    @ManyToOne
+    private Customer customer;
+
+   @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+   @JoinColumn(name = "staff_id",nullable = false)
+    @ManyToOne
+    private Staff staff;
+    @Column(name = "last_update",nullable = false)
+    private LocalDateTime lastUpdate;
+    @PrePersist
+    @PreUpdate
+    private void onUpdate() {
+        lastUpdate = LocalDateTime.now();
+    }
+
+    public Rental(LocalDateTime rentalDate, Inventory inventory, Customer customer, LocalDateTime returnDate, Staff staff, LocalDateTime lastUpdate) {
+        this.rentalDate = rentalDate;
+        this.inventory = inventory;
+        this.customer = customer;
+        this.returnDate = returnDate;
+        this.staff = staff;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Rental() {
+    }
+
+    public Integer getRentalId() {
+        return rentalId;
+    }
+
+    public void setRentalId(Integer rentalId) {
+        this.rentalId = rentalId;
+    }
+
+    public LocalDateTime getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(LocalDateTime rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
+
