01b56b84bf2d09f668d3cb8b8ca9c3c810f38240
inuyasha <yingzijiany@gmail.com>
2024-04-15 14:46:34 +0200

    language entity fertig

--- src/main/java/de/ostfalia/bootablejarstarter/entity/Language.java
+++ src/main/java/de/ostfalia/bootablejarstarter/entity/Language.java
@@ -2,6 +2,8 @@ package de.ostfalia.bootablejarstarter.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "language")
 public class Language {
@@ -10,4 +12,45 @@ public class Language {
     @Column(name = "language_id",nullable = false)
     private Integer languageId;
 
+    @Column(length = 20,nullable = false)
+    private String name;
+    @Column(name = "last_update",nullable = false)
+    private LocalDateTime lastUpdate;
+    @PrePersist
+    @PreUpdate
+    private void onUpdate() {
+        lastUpdate = LocalDateTime.now();
+    }
+
+    public Language() {
+    }
+
+    public Language(String name, LocalDateTime lastUpdate) {
+        this.name = name;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Integer getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Integer languageId) {
+        this.languageId = languageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
