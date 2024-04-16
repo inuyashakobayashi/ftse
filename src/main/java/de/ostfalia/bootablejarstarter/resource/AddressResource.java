ca468080c53cd149d31ae2ed25353eb6ebaf6d76
inuyasha <yingzijiany@gmail.com>
2024-04-16 20:24:27 +0200

    Aufsetztung der datasource mit cli und persistence.xml

--- src/main/java/de/ostfalia/bootablejarstarter/resource/AddressResource.java
+++ src/main/java/de/ostfalia/bootablejarstarter/resource/AddressResource.java
@@ -1,8 +1,26 @@
 package de.ostfalia.bootablejarstarter.resource;
 
+import de.ostfalia.bootablejarstarter.entity.Address;
+import de.ostfalia.bootablejarstarter.service.AddressService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
 
 @Path("/addresses")
 public class AddressResource {
+    @Inject
+    AddressService addressService;
+    @GET
+    @Path("{page}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAddress(@PathParam("page") Integer page){
+        return Response.ok(addressService.getAddressListWithPage(page)).build();
 
+    }
 }
--- src/main/java/de/ostfalia/bootablejarstarter/service/AddressService.java
+++ src/main/java/de/ostfalia/bootablejarstarter/service/AddressService.java
@@ -4,11 +4,17 @@ import de.ostfalia.bootablejarstarter.entity.Address;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
 @Stateless
 public class AddressService {
-    @PersistenceContext
+    @PersistenceContext(unitName = "customer")
     EntityManager em;
 
+    private static final int PAGE_SIZE = 10;
     public void save(Address address){
         em.persist(address);
     }
@@ -24,4 +30,16 @@ public class AddressService {
     public void delete(Address address){
         em.remove(address);
     }
+    public List<Address> getAddressList(){
+        TypedQuery<Address> query = em.createQuery("SELECT a from Address a", Address.class);
+        return query.getResultList();
+    }
+public List<Address> getAddressListWithPage(Integer page){
+
+    TypedQuery<Address> query = em.createQuery("SELECT a from Address a", Address.class);
+    query.setFirstResult((page-1)*PAGE_SIZE);
+    query.setMaxResults(PAGE_SIZE);
+    return query.getResultList();
+}
+
 }
