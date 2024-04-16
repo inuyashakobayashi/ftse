697647075b9caeda820fd1d1dae7ce4053997ad6
inuyasha <yingzijiany@gmail.com>
2024-04-16 23:50:04 +0200

    get address mit page fertig

--- src/main/java/de/ostfalia/bootablejarstarter/resource/AddressResource.java
+++ src/main/java/de/ostfalia/bootablejarstarter/resource/AddressResource.java
@@ -3,10 +3,8 @@ package de.ostfalia.bootablejarstarter.resource;
 import de.ostfalia.bootablejarstarter.entity.Address;
 import de.ostfalia.bootablejarstarter.service.AddressService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.validation.constraints.Min;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -17,9 +15,9 @@ public class AddressResource {
     @Inject
     AddressService addressService;
     @GET
-    @Path("{page}")
+
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAddress(@PathParam("page") Integer page){
+    public Response getAddress(@Min(value = 1 ,message = "muss größer als 0 sein")@DefaultValue ("1")@QueryParam("page") Integer page){
         return Response.ok(addressService.getAddressListWithPage(page)).build();
 
     }
